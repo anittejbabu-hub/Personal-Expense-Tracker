8b0a63949b69dd483a4be09e28f19a011348b215
anittejbabu-hub <anittejbabu@gmail.com>
2026-02-18 14:45:00 +0530

    Updated main.py with saving feature
    Added functionality to view and save expenses in a JSON file.

--- main.py
+++ main.py
@@ -1,17 +1,49 @@
+import json
+
 print("Welcome to Personal Expense Tracker")
 
 while True:
     print("\n1. Add Expense")
-    print("2. Exit")
+    print("2. View Expenses")
+    print("3. Exit")
 
     choice = input("Enter choice: ")
 
     if choice == "1":
         amount = input("Enter amount: ")
         category = input("Enter category: ")
-        print("Expense Added:", amount, category)
+
+        expense = {
+            "amount": amount,
+            "category": category
+        }
+
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+        except:
+            data = []
+
+        data.append(expense)
+
+        with open("data.json", "w") as file:
+            json.dump(data, file, indent=4)
+
+        print("Expense Saved Successfully!")
 
     elif choice == "2":
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+
+            print("\nAll Expenses:")
+            for expense in data:
+                print(expense)
+
+        except:
+            print("No expenses found.")
+
+    elif choice == "3":
         print("Goodbye!")
         break
 
